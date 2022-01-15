904aa7a3373d73a17a96c8bcbbaa32465151c36c
rafat-darawshe <king.darwshe@gmail.com>
2022-01-15 14:14:11 +0200

    123

---
+++ WEB-FUNDAMENTALS/JavaScript/Loop Challenges/LoopChallenges.js
@@ -0,0 +1,32 @@
+for(var i = 1; i<20; i+=2){
+    console.log(i);
+}
+
+///////////////////////////////////////////
+
+for(var i = 100; i >= 0; i--){
+    if(i % 3 == 0){
+        console.log(i);
+    }
+}
+
+///////////////////////////////////////////
+
+for(var i = 4; i > -4; i-=1.5){
+        console.log(i);
+}
+
+///////////////////////////////////////////
+
+sum = 0
+for(var i = 0; i <= 100; i++){
+    sum = sum+i
+} console.log(sum)
+
+///////////////////////////////////////////
+
+product = 1;
+for(var i=1; i <= 12; i++){
+    product = product*i
+}
+console.log(product);
