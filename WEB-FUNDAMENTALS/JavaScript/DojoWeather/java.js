bf14b3a71b49f84cc40a0b8a9f7630985d0b6b8c
rafat-darawshe <king.darwshe@gmail.com>
2022-01-17 18:11:49 +0200

    123

---
+++ WEB-FUNDAMENTALS/JavaScript/DojoWeather/java.js
@@ -0,0 +1,29 @@
+function ale() {
+    alert("Lading weather report....")
+}
+var remo=document.querySelector("article") 
+function accept(){
+    remo.remove();
+}
+
+
+function c2f(temp) {
+    return Math.round(9 / 5 * temp + 32);
+}
+
+function f2c(temp) {
+    return Math.round(5 / 9 * (temp - 32));
+}
+
+function convert(element) {
+    console.log(element.value);
+    for(var i=1; i<9; i++) {
+        var tempSpan = document.querySelector("#temp" + i);
+        var tempVal = parseInt(tempSpan.innerText);
+        if(element.value == "°C") {
+            tempSpan.innerText = f2c(tempVal);
+        } else {
+            tempSpan.innerText = c2f(tempVal);
+        }
+    }
+}
---
+++ WEB-FUNDAMENTALS/JavaScript/Making Connections/ja.js
@@ -0,0 +1,16 @@
+var rm=document.querySelector(".user-line")
+var row=document.querySelector(".user-line1")
+var na=document.querySelector(".card-body h1")
+function remove(){
+    rm.remove();
+}
+
+function rem(){
+    row.remove();
+
+}
+function run(){
+
+    na.innerText="Darawshe"
+
+}
