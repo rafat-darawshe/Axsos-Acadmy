4554acb02f1193147040d1f42d76bc7da07cf006
rafat-darawshe <king.darwshe@gmail.com>
2022-01-30 14:10:56 +0200

    123

---
+++ Python/For Loops Basic I/for_loop_basic1.py
@@ -0,0 +1,39 @@
+#1
+for x in range(0, 151, 1):
+    print(x)
+
+#2
+for x in range(5, 1000, 5):
+    print(x+5)
+
+#3
+for x in range(0, 100+1):
+    if x % 10 == 0:
+        print("Coding Dojo")
+   
+    elif x % 5 == 0:
+        print("Coding")   
+
+    else:
+     print(x)
+
+#4
+sum = 0
+for x in range(1,500000,2):
+    sum += x
+print(sum)
+
+#5
+for x in range(2018,0,-4):
+    print(x)
+
+
+#6
+lowNum = 2
+highNum = 9
+mult = 3
+for x in range(lowNum,highNum+1):
+    if x % mult == 0:
+        print(x)
+
+
