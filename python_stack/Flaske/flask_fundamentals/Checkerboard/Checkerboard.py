eb687c34130d1f3d7ee18a4d2daa2727f678baa6
rafat-darawshe <king.darwshe@gmail.com>
2022-02-05 13:49:24 +0200

    -___-

--- python_stack/Flaske/flask_fundamentals/Checkerboard/Checkerboard.py
+++ python_stack/Flaske/flask_fundamentals/Checkerboard/Checkerboard.py
@@ -1,10 +1,20 @@
-from os import times
-from flask import Flask, render_template  
-app = Flask(__name__)                     
-    
-@app.route('/a')                           
-def hello():
-    return render_template('Checkerboard.html',)  
+from flask import Flask, render_template
+app = Flask(__name__)
 
-if __name__=="__main__":
-    app.run(debug=True)    
+@app.route('/')
+def index():
+    return render_template('Checkerboard.html', across = 8, down = 8)
+
+@app.route('/<num>')
+def callDown(num):
+    return render_template('Checkerboard.html', across = 8, down = int(num))
+
+@app.route('/<num1>/<num2>')
+def acrossDown(num1,num2):
+    return render_template('Checkerboard.html', across = int(num1), down = int(num2) )
+
+@app.route('/<num1>/<num2>/<col1>/<col2>')
+def hellaColors(num1, num2, col1, col2):
+    return render_template('Checkerboard.html', across = int(num1), down = int(num2), col1= col1, col2 = col2)
+if __name__ == "__main__":
+    app.run(debug=True)
