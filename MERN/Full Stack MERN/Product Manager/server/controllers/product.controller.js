9f41708db717afa86a886fb0ecb36b8058e05807
rafat-darawshe <king.darwshe@gmail.com>
2022-04-19 14:38:25 +0300

    full stack mern first commit

---
+++ MERN/Full Stack MERN/Product Manager/server/controllers/product.controller.js
@@ -0,0 +1,18 @@
+const { Product } = require('../models/Product.model');
+module.exports.index = (request, response) => {
+    response.json({
+        message: "Hello World"
+    });
+}
+    // The method below is new
+module.exports.createProduct = (request, response) => {
+    const { title, price, description } = request.body;
+    Product.create({
+        title,
+        price,
+        description
+    })
+        .then(Product => response.json(Product))
+        .catch(err => response.json(err));
+}
+
